263f33e089390550f5ae1bfd60bf704b32e74711
thanhbebi17 <thanhbebi1710@yahoo.com>
2016-06-15 21:30:32 +0700

    Add exercise api

--- PowerfulTrainer/PowerfulTrainer.Web/Controllers/Api/ExerciseController.cs
+++ PowerfulTrainer/PowerfulTrainer.Web/Controllers/Api/ExerciseController.cs
@@ -11,9 +11,78 @@ using Windows.Storage.Streams;
 
 namespace PowerfulTrainer.Web.Controllers.Api
 {
-    public class ExerciseController : BaseApiController
+    public class ExerciseController : CheckTokenController
     {
-        
-        
+        [Route("api/exercise")]
+        [HttpGet]
+        public object Get(String BodyPart = null, string Level=null, string Focus = null)
+        {
+            try
+            {
+                var ListExercis = DB.Exercises.Where(u => true);
+                if (BodyPart != null)
+                {
+                    ListExercis = ListExercis.Where(u => u.BodyParts == BodyPart);
+                }
+                if (Level != null)
+                {
+                    ListExercis = ListExercis.Where(u => u.DifficultyLevel == Level);
+                }
+                if (Focus != null)
+                {
+                    ListExercis = ListExercis.Where(u => u.Focus.Contains(Focus));
+                }
+                return SuccessResult(ListExercis.ToArray());
+            }
+            catch(Exception ex)
+            {
+                return FailResult(ex);
+            }
+        }
+
+        [Route("api/exercise/level")]
+        [HttpGet]
+        public object GetLevel()
+        {
+            try
+            {
+                return SuccessResult(DB.Exercises.Select(u=>u.DifficultyLevel).Distinct().Where(u=>!string.IsNullOrEmpty(u)).ToArray());
+            }
+            catch (Exception ex)
+            {
+                return FailResult(ex);
+            }
+        }
+
+        [Route("api/exercise/focus")]
+        [HttpGet]
+        public object GetFocus()
+        {
+            try
+            {
+                var Focus = DB.Exercises.Select(u => u.Focus).Distinct().Where(u => !string.IsNullOrEmpty(u)).ToList();
+                var ListFocus = new List<string>();
+                Focus.ForEach(u => u.Split(',').ToList().ForEach(v => ListFocus.Add(v.Trim())));
+                return SuccessResult(ListFocus.Distinct().ToArray());
+            }
+            catch (Exception ex)
+            {
+                return FailResult(ex);
+            }
+        }
+
+        [Route("api/exercise/bodypart")]
+        [HttpGet]
+        public object GetBodyPart()
+        {
+            try
+            {
+                return SuccessResult(DB.Exercises.Select(u => u.BodyParts).Distinct().Where(u => !string.IsNullOrEmpty(u)).ToArray());
+            }
+            catch (Exception ex)
+            {
+                return FailResult(ex);
+            }
+        }
     }
 }
