99f16cc3a039e32765b2242381e429083eb1872f
thanhbebi17 <thanhbebi1710@yahoo.com>
2016-06-15 22:13:50 +0700

    Add PlanData Model

---
+++ PowerfulTrainer/PowerfulTrainer/Models/PlanData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerfulTrainer.Models
+{
+    public class PlanItem
+    {
+        public int ExerciseId;
+        public int Sets;
+        public int Repetitions;
+        public string Duration;
+        public string RestTime;
+        public bool IsRestItem;
+    }
+    public class PlanData : List<PlanItem> { }
+}
