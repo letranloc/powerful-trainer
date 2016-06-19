b9b9895e31f33b7fed5514c96bb36abcfad8fdf8
letranloc <letranloc180@gmail.com>
2016-06-19 10:29:02 +0700

    [web] remove lib

--- PowerfulTrainer/PowerfulTrainer.Web/wwwroot/lib/angular-material-sidenav-menu/src/main/resources/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-var module = angular.module('App', ['ngMaterial', 'ngMenuSidenav']);
-
-module.controller('AppCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
-    $scope.index = 0;
-
-    $scope.toggleSidenav = function (menuId) {
-        $mdSidenav(menuId).toggle();
-    };
-}]);
