c0dbd0bb9cbed07a1b375a999356c29f622a68a7
letranloc <letranloc180@gmail.com>
2016-06-19 10:26:01 +0700

    [web] rebuild

--- PowerfulTrainer/PowerfulTrainer.Web/wwwroot/_references.js
+++ PowerfulTrainer/PowerfulTrainer.Web/wwwroot/_references.js
@@ -1,2 +1,5 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
+/// <reference path="js/app.js" />
+/// <reference path="js/libs.js" />
+/// <reference path="js/site.min.js" />
