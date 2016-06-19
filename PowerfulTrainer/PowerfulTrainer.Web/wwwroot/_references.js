fd85473a3cdd274e360b9a1827c54ce29de9e4ca
letranloc <letranloc180@gmail.com>
2016-06-19 10:17:37 +0700

    [web] remove unnecessary files

--- PowerfulTrainer/PowerfulTrainer.Web/wwwroot/_references.js
+++ PowerfulTrainer/PowerfulTrainer.Web/wwwroot/_references.js
@@ -1,5 +1,2 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
-/// <reference path="js/app.js" />
-/// <reference path="js/libs.js" />
-/// <reference path="js/site.min.js" />
