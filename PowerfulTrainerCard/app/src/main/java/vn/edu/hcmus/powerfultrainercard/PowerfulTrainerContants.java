21c2d7badaf2950dcd6e061373f2f4d8999e236c
letranloc <letranloc180@gmail.com>
2016-06-20 08:11:49 +0700

    [android] add powerful trainer card project

---
+++ PowerfulTrainerCard/app/src/main/java/vn/edu/hcmus/powerfultrainercard/PowerfulTrainerContants.java
@@ -0,0 +1,8 @@
+package vn.edu.hcmus.powerfultrainercard;
+
+public abstract class PowerfulTrainerContants {
+
+    public static final String VUFORIA_KEY = "ARgBF2v/////AAAAAZFpAWKmyUcZp9Y8DRGGd9dVprE6yJInl1IttRtsdFWY8MtkmC1vAD2OC8/H981GR+TfXnGEKU+8CZto6kgonZ1jNCEG/cWGQhrwnVBH7WYt6at2B4Ri0CFnaHsbGjZpBPYiAa4Kin0OijKiwM9+C2SOrQ5ZuVRK6DVEBGShAybZ2Ap+0o5qQ6pOUtUP6ho6S0D0XOd0fEXscWbYCnd6jcgRXLmg9wDm8y2nD7I+yLMndA0aZHkrFYgeMqWfgiIwKmA1pNx+5lWZ0oFxHaei68qCdwzI3SVBUSX+VhSFUwU20O0up72hW4UlDbURT/5pcVDBDMbAJm4j5MC1CsGjwH/dK3PPr5sfYOaczatzJQEx";
+    public static final String CLOUD_ACCESS_KEY = "c7dc63e4b293c5262f491f600357a5e4adb7f039";
+    public static final String CLOUD_SECRET_KEY = "9a7fd5e6bc2ff0b503168db965aa65534ea77a88";
+}
